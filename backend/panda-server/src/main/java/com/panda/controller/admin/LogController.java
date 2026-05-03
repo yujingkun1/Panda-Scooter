943725f9a072c817137bf374c1574f482d2e6d50
lkc <2146870998@qq.com>
2026-05-03 13:43:54 +0800

    登录

---
+++ backend/panda-server/src/main/java/com/panda/controller/admin/LogController.java
@@ -0,0 +1,31 @@
+package com.panda.controller.admin;
+
+import com.panda.dto.AdminLoginDTO;
+import com.panda.result.Result;
+import com.panda.service.AdminService;
+import com.panda.vo.AdminLoginVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/admin/log")
+@Slf4j
+public class LogController {
+    @Autowired
+    private AdminService adminService;
+
+    /**
+     * 管理员登录
+     */
+    @PostMapping("/login")
+    public Result<AdminLoginVO> login(@RequestBody AdminLoginDTO loginDTO) {
+        log.info("管理员登录请求，邮箱：{}", loginDTO.getEmail());
+        AdminLoginVO adminLoginVO = adminService.login(loginDTO);
+        return Result.success(adminLoginVO);
+    }
+
+
+}
